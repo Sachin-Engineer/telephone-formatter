2a92fb4689e269dceceabcbad38c9da43324c15f
Sachin-Engineer <sachin.engineer.22@gmail.com>
2025-08-02 14:49:58 +0530

    second commit

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 const input = document.querySelector('input')
 
+let alreadyRun = false;
+
 input.addEventListener('input', () => {
-    if(input.value.length == 4) {
+    let conditionIsTrue = true;
+
+    if(input.value.length == 4 && conditionIsTrue && !alreadyRun) {
         const countryCode = input.value.slice(0, 3)
         console.log(countryCode)
         const phoneNumber = input.value.slice(3, input.value.length+1)
         console.log(phoneNumber)
         input.value = `+(${countryCode}) - ${phoneNumber}`
+        alreadyRun = true;
     }
     if(input.value.length > 10 && input.value.includes('+')) {
         const countryCode = input.value.slice(2, 5)
@@ -14,5 +19,8 @@ input.addEventListener('input', () => {
         const phoneNumber = input.value.slice(9, input.value.length+1)
         console.log(phoneNumber)
         input.value = `+(${countryCode}) - ${phoneNumber}`
+    }
+    if(input.value.length < 4) {
+        alreadyRun = false;
     }
 })
